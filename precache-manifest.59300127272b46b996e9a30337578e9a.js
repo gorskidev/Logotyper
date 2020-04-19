967ecd40e94566f47062dcc5a8026fe088d94762
Maciek <maciej.gorski995@gmail.com>
2020-04-19 19:21:40 +0200

    Updates

---
+++ precache-manifest.59300127272b46b996e9a30337578e9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b9dd608ce9cbba2f396eaa819a8209",
+    "url": "/Logotyper/index.html"
+  },
+  {
+    "revision": "0fd756797c3f55e65853",
+    "url": "/Logotyper/static/css/main.b9e41dcf.chunk.css"
+  },
+  {
+    "revision": "f00b15e5c7293ac07181",
+    "url": "/Logotyper/static/js/2.5d9d8c8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Logotyper/static/js/2.5d9d8c8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd756797c3f55e65853",
+    "url": "/Logotyper/static/js/main.0c801e1f.chunk.js"
+  },
+  {
+    "revision": "b1723b01c7428e8933d7",
+    "url": "/Logotyper/static/js/runtime-main.87134ae6.js"
+  },
+  {
+    "revision": "2ca4aa2af3d7eaf9a626395a2a9d7787",
+    "url": "/Logotyper/static/media/character.2ca4aa2a.png"
+  },
+  {
+    "revision": "92ac593df7052e9f00ccf94985cf94d7",
+    "url": "/Logotyper/static/media/desktop-art.92ac593d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.268ced7a58334ec13322c688ef557348.js"
+  "/Logotyper/precache-manifest.59300127272b46b996e9a30337578e9a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Logotyper/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
